ddd039153c94cfeec6a67521de67740086b9d457
Dadsamevolu137 <silveiradavid2009@gmail.com>
2026-05-13 19:04:38 -0300

    fix porta mysql

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ load_dotenv()
 def get_connection():
     return mysql.connector.connect(
         host=os.getenv("MYSQL_HOST", "localhost"),
-        port=int(os.getenv("MYSQL_PORT", 3306)),
+        port=int(str(os.getenv("MYSQL_PORT", "3306")).strip()),
         user=os.getenv("MYSQL_USER", "root"),
         password=os.getenv("MYSQL_PASSWORD", ""),
         database=os.getenv("MYSQL_DATABASE", "defaultdb"),
